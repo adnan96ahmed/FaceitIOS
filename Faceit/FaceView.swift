75d182fc7fb3ed1598c38d29f9ae3e6cfbafc9be
Adnan Ahmed <adnan_a96@hotmail.com>
2017-12-24 06:24:16 -0500

    program draws a circle

--- Faceit/FaceView.swift
+++ Faceit/FaceView.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class FaceView: UIView {
 
-
+    var scale: CGFloat = 0.9
+    
+    private var skullRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    private var skullCentre: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    private func pathForSkull() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: skullCentre, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        return path
+    }
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        UIColor.blue.set()
+        pathForSkull().stroke()
     }
 
 }
