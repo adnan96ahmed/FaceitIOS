a86826aa5ad429421579074c687ee67e8da80024
Adnan Ahmed <adnan_a96@hotmail.com>
2017-12-25 18:58:57 -0500

    added tap function for eyes

--- Faceit/ViewController.swift
+++ Faceit/ViewController.swift
@@ -15,7 +15,18 @@ class ViewController: UIViewController {
             let handler = #selector(FaceView.changeScale(byReactingTo:))
             let pinchRecognizer = UIPinchGestureRecognizer(target: faceView, action: handler)
             faceView.addGestureRecognizer(pinchRecognizer)
+            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byReactingTo:)))
+            tapRecognizer.numberOfTapsRequired = 1
+            faceView.addGestureRecognizer(tapRecognizer)
             updateUI()
+        }
+    }
+    
+    @objc
+    public func toggleEyes(byReactingTo tapRecognizer: UITapGestureRecognizer) {
+        if tapRecognizer.state == .ended {
+            let eyes: FacialExpression.Eyes = (expression.eyes == .closed) ? .open : .closed
+            expression = FacialExpression(eyes: eyes, mouth: expression.mouth)
         }
     }
     
